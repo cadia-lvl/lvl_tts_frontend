866e229e3a859038c0b3d8c8924dd9e1661d8a53
Anna Nikulasdottir <anna.b.nik@gmx.de>
2018-05-22 13:43:02 +0000

    postaspir check

--- pron_dict_quality_assurance/post_aspiration.py
+++ pron_dict_quality_assurance/post_aspiration.py
@@ -7,6 +7,118 @@
 import sys, csv, re
 
 
+def write_list(filename, list2write):
+
+    with open(filename, 'w') as f:
+        for line in list2write:
+            f.write(line)
+
+
+def find_postaspir(input_file):
+
+    c_postaspir = []
+    k_postaspir = []
+    p_postaspir = []
+    t_postaspir = []
+
+    for line in input_file:
+        word, transcr = line.strip().split('\t')
+        if re.search('cʰ', transcr):
+            c_postaspir.append(line)
+        if re.search('kʰ', transcr):
+            k_postaspir.append(line)
+        if re.search('pʰ', transcr):
+            p_postaspir.append(line)
+        if re.search('tʰ', transcr):
+            t_postaspir.append(line)
+
+    write_list('c_postaspir.txt', c_postaspir)
+    write_list('k_postaspir.txt', k_postaspir)
+    write_list('p_postaspir.txt', p_postaspir)
+    write_list('t_postaspir.txt', t_postaspir)
+
+
+def find_beginning_postaspir(input_file):
+    c_postaspir = []
+    k_postaspir = []
+    p_postaspir = []
+    t_postaspir = []
+
+    for line in input_file:
+        word, transcr = line.strip().split('\t')
+        if re.match('cʰ.+', transcr):
+            c_postaspir.append(line)
+        if re.match('kʰ.+', transcr):
+            k_postaspir.append(line)
+        if re.match('pʰ.+', transcr):
+            p_postaspir.append(line)
+        if re.match('tʰ.+', transcr):
+            t_postaspir.append(line)
+
+    write_list('c_beginningpostaspir.txt', c_postaspir)
+    write_list('k_beginningpostaspir.txt', k_postaspir)
+    write_list('p_beginningpostaspir.txt', p_postaspir)
+    write_list('t_beginningpostaspir.txt', t_postaspir)
+
+
+
+def find_missing_postaspir(input_file):
+    c_postaspir = []
+    k_postaspir = []
+    p_postaspir = []
+    t_postaspir = []
+
+    for line in input_file:
+        word, transcr = line.split('\t')
+        if re.match('[pP][aeiouyáéíóúýöæjlrv].*', word):
+            if not re.match('pʰ.+', transcr):
+                p_postaspir.append(line)
+        if re.match('[tT][aeiouyáéíóúýöæjrv].*', word):
+            if not re.match('tʰ.+', transcr):
+                t_postaspir.append(line)
+        if re.match('[kK][aouáóúölrv].*', word):
+            if not re.match('kʰ.+', transcr):
+                k_postaspir.append(line)
+        if re.match('[hH]v[aeiouyáéíóúýöæjr].*', word):
+            if not re.match('kʰ.+', transcr):
+                k_postaspir.append(line)
+        if re.match('[kK][eiéíyýæj].*', word):
+            if not re.match('cʰ.+', transcr):
+                c_postaspir.append(line)
+
+    write_list('c_missingpostaspir.txt', c_postaspir)
+    write_list('k_missingpostaspir.txt', k_postaspir)
+    write_list('p_missingpostaspir.txt', p_postaspir)
+    write_list('t_missingpostaspir.txt', t_postaspir)
+
+
+def ensure_postaspir(input_file):
+    for line in input_file:
+        word, transcr = line.split('\t')
+        if re.match('[pP][aeiouyáéíóúýöæjlrv].*', word):
+            if not re.match('pʰ.+', transcr):
+                new_ipa = transcr.replace('p', 'pʰ', 1)
+                transcr = new_ipa
+        if re.match('[tT][aeiouyáéíóúýöæjrv].*', word):
+            if not re.match('tʰ.+', transcr):
+                new_ipa = transcr.replace('t', 'tʰ', 1)
+                transcr = new_ipa
+        if re.match('[kK][aouáóúölrv].*', word):
+            if not re.match('kʰ.+', transcr):
+                new_ipa = transcr.replace('k', 'kʰ', 1)
+                transcr = new_ipa
+        if re.match('[hH]v[aeiouyáéíóúýöæ].*', word):
+            if not re.match('kʰ.+', transcr):
+                new_ipa = transcr.replace('k', 'kʰ', 1)
+                transcr = new_ipa
+        if re.match('[kK][eiéíyýæj].*', word):
+            if not re.match('cʰ.+', transcr):
+                new_ipa = transcr.replace('c', 'cʰ', 1)
+                transcr = new_ipa
+
+        print(word + '\t' + transcr.strip())
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='post aspiration',
@@ -18,26 +130,10 @@ def main():
 
     args = parser.parse_args()
 
-    for line in args.input:
-        line_list = line.split('\t')
-        if re.match('[pP][aeiouyáéíóúýöæjrv].*', line):
-            if not re.match('pʰ.+', line_list[1]):
-                new_ipa = line_list[1].replace('p', 'pʰ', 1)
-                line_list[1] = new_ipa
-        if re.match('[tT][aeiouyáéíóúýöæjrv].*', line):
-            if not re.match('tʰ.+', line_list[1]):
-                new_ipa = line_list[1].replace('t', 'tʰ', 1)
-                line_list[1] = new_ipa
-        if re.match('[kK][aouáóúörv].*', line):
-            if not re.match('kʰ.+', line_list[1]):
-                new_ipa = line_list[1].replace('k', 'kʰ', 1)
-                line_list[1] = new_ipa
-        if re.match('[kK][eiéíyýæj].*', line):
-            if not re.match('cʰ.+', line_list[1]):
-                new_ipa = line_list[1].replace('c', 'cʰ', 1)
-                line_list[1] = new_ipa
-
-        print(('\t').join(line_list).strip())
+    #find_postaspir(args.input)
+    #find_missing_postaspir(args.input)
+    #find_beginning_postaspir(args.input)
+    ensure_postaspir(args.input)
 
 
 if __name__ == '__main__':
