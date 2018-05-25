9a9c346e9c17486e02c1176fde9a4af3ab276497
Anna Nikulasdottir <anna.b.nik@gmx.de>
2018-05-25 14:27:19 +0000

    wordlist remove

---
+++ pron_dict_quality_assurance/wordlist_remover.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Remove words from a word list from a dictionary.
+Write out a new dictionary.
+
+input format dictionary:
+
+word    t r a n s c r i p t i o n
+...
+
+
+
+"""
+
+import sys
+
+frob_file = sys.argv[1]
+word_list_file = sys.argv[2]
+
+word_list = []
+
+for line in open(word_list_file).readlines():
+    word_list.append(line.strip())
+
+result_dictionary = []
+
+for line in open(frob_file):
+    word, transcr = line.strip().split('\t')
+    if word not in word_list:
+        result_dictionary.append(line)
+
+with open(frob_file + '_filtered.txt', 'w') as f:
+    for item in result_dictionary:
+        f.write(item)
