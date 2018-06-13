87d37f46d6f94b8be846924c65f2a6f34cc3e412
Anna Nikulasdottir <anna.b.nik@gmx.de>
2018-06-13 11:13:52 +0000

    create class in g2p

--- pron_dict_quality_assurance/grapheme_phoneme_mapping.py
+++ pron_dict_quality_assurance/grapheme_phoneme_mapping.py
@@ -6,18 +6,98 @@ import sys
 SHORT_VOWELS = ['a', 'E', 'I', 'i', 'O', 'Y', 'u', '9', 'ai', 'ei', 'Ou', '9Y']
 LONG_VOWELS = ['a:', 'E:', 'I:', 'i:', 'O:', 'Y:', 'u:', '9:', 'ai:', 'ei:', 'Ou:', '9Y:']
 
-def map_g2p(word, transcript):
-    word_arr = list(word)
-    tr_arr = transcript.split()
 
-    g2p_tuples = []
-    if len(word_arr) != len(tr_arr):
-        return g2p_tuples
+class G2P_align:
 
-    for ind, c in enumerate(word_arr):
-        g2p_tuples.append((c.lower(), tr_arr[ind]))
+    def __init__(self, prondict_list, min_occur=1000):
+        self.g2p_map = {}
+        self.one_on_one_map = self.init_map(prondict_list)
+        self.init_g2p_map(self.one_on_one_map, min_occur)
+        self.add_special_mappings()
 
-    return g2p_tuples
+    def init_map(self, p_list):
+        one_on_one_map = {}
+        for line in p_list:
+            word, transcr = line.strip().split('\t')
+            tuples = self.map_g2p_one_on_one(word, transcr)
+            for t in tuples:
+                if t in one_on_one_map:
+                    one_on_one_map[t] = one_on_one_map[t] + 1
+                else:
+                    one_on_one_map[t] = 1
+        return one_on_one_map
+
+    def map_g2p_one_on_one(self, word, transcript):
+        """
+        Get basic g2p mapping, only create mappings for word-transcript pairs that are equally long.
+        :param word:
+        :param transcript:
+        :return: a list of g2p tuples if mapped, an empty list otherwise
+        """
+        word_arr = list(word)
+        tr_arr = transcript.split()
+
+        tuples = []
+        if len(word_arr) != len(tr_arr):
+            return tuples
+        for ind, c in enumerate(word_arr):
+            tuples.append((c.lower(), tr_arr[ind]))
+
+        return tuples
+
+    def init_g2p_map(self, map_to_filter, min_occur):
+        self.g2p_map = {}
+        for t in map_to_filter.keys():
+            if map_to_filter[t] > min_occur:
+                if t[0] in self.g2p_map:
+                    self.g2p_map[t[0]].append(t[1])
+                else:
+                    self.g2p_map[t[0]] = [t[1]]
+
+    def extend_mapping(self, prondict_list, min_occur=100):
+        extended_g2p_map = {}
+        for line in prondict_list:
+            word, transcr = line.strip().split('\t')
+            aligned = align_g2p(word, transcr, self.g2p_map)
+
+            for t in aligned:
+                if t in extended_g2p_map:
+                    extended_g2p_map[t] = extended_g2p_map[t] + 1
+                else:
+                    extended_g2p_map[t] = 1
+
+        self.init_g2p_map(extended_g2p_map, min_occur)
+        self.add_special_mappings()
+
+    def add_special_mappings(self):
+        if 'i' in self.g2p_map:
+            self.g2p_map['y'] = self.g2p_map['i']
+        if 'í' in self.g2p_map:
+            self.g2p_map['ý'] = self.g2p_map['í']
+
+        # ensure short and long versions of vowels
+        for grapheme in self.g2p_map.keys():
+            for p in self.g2p_map[grapheme]:
+                if p in SHORT_VOWELS:
+                    if p + ':' not in self.g2p_map[grapheme]:
+                        self.g2p_map[grapheme].append(p + ':')
+                elif p in LONG_VOWELS:
+                    short = p.replace(':', '')
+                    if short not in self.g2p_map[grapheme]:
+                        self.g2p_map[grapheme].append(short)
+
+        # manually add dipththongs and other special cases:
+        self.g2p_map['ei'] = ['ei', 'ei:']
+        self.g2p_map['ey'] = ['ei', 'ei:']
+        self.g2p_map['au'] = ['9Y', '9Y:']
+        self.g2p_map['hj'] = ['C']
+        self.g2p_map['hl'] = ['l_0']
+        self.g2p_map['hr'] = ['r_0']
+        self.g2p_map['hn'] = ['n_0']
+        self.g2p_map['sl'] = ['s t l']
+        self.g2p_map['x'] = ['k s']
+        self.g2p_map['é'] = ['j E', 'j E:']
+
 
 def set_alignment(c, word_arr, tr_arr, g_anchor, p_anchor, g_ind, p_ind, g2p_tuples):
     graphemes = ''.join(word_arr[g_anchor + 1: g_ind])
@@ -164,146 +244,22 @@ def align_g2p(word, transcript, g2p_map):
     return g2p_tuples
 
 
-
-
 def main():
 
     pron_dict_in = open(sys.argv[1]).readlines()
-
-    tmp_g2p_map = {}
-    for line in pron_dict_in:
-        word, transcr = line.strip().split('\t')
-
-        result = map_g2p(word, transcr)
-        #if len(result) == 0:
-        #    print(line.strip())
-
-        for t in result:
-            if t in tmp_g2p_map:
-                tmp_g2p_map[t] = tmp_g2p_map[t] + 1
-            else:
-                tmp_g2p_map[t] = 1
-
-    g2p_map = {}
-    for t in tmp_g2p_map.keys():
-        if tmp_g2p_map[t] > 1000:
-            if t[0] in g2p_map:
-                g2p_map[t[0]].append(t[1])
-            else:
-                g2p_map[t[0]] = [t[1]]
-
-    if 'i' in g2p_map:
-        g2p_map['y'] = g2p_map['i']
-    if 'í' in g2p_map:
-        g2p_map['ý'] = g2p_map['í']
-
-    # ensure short and long versions of vowels
-    for grapheme in g2p_map.keys():
-        for p in g2p_map[grapheme]:
-            if p in SHORT_VOWELS:
-                if p + ':' not in g2p_map[grapheme]:
-                    g2p_map[grapheme].append(p + ':')
-            elif p in LONG_VOWELS:
-                short = p.replace(':','')
-                if short not in g2p_map[grapheme]:
-                    g2p_map[grapheme].append(short)
-
-
-    #manually add dipththongs and h: k for 'hv':
-    g2p_map['ei'] = ['ei', 'ei:']
-    g2p_map['ey'] = ['ei', 'ei:']
-    g2p_map['au'] = ['9Y', '9Y:']
-    g2p_map['hj'] = ['C']
-    g2p_map['hl'] = ['l_0']
-    g2p_map['hr'] = ['r_0']
-    g2p_map['hn'] = ['n_0']
-    g2p_map['sl'] = ['s t l']
+    g2p = G2P_align(pron_dict_in, 1000)
+    g2p.extend_mapping(pron_dict_in)
 
     tmp_g2p_map = {}
     g2p_map_v2 = {}
     aligned_dict = []
     for line in pron_dict_in:
         word, transcr = line.strip().split('\t')
-        aligned = align_g2p(word, transcr, g2p_map)
+        aligned = align_g2p(word, transcr, g2p.g2p_map)
 
         for t in aligned:
             if t in tmp_g2p_map:
                 tmp_g2p_map[t].append(word + '  ---  ' + transcr)
-                """
-                if len(tmp_g2p_map[t]) > 100:
-                    if t[0] in g2p_map:
-                        g2p_map[t[0]].append(t[1])
-                    else:
-                        g2p_map[t[0]] = [t[1]]
-                """
-
-            else:
-                tmp_g2p_map[t] = [word + '  ---  ' + transcr]
-
-            if t in g2p_map_v2:
-                g2p_map_v2[t] = g2p_map_v2[t] + 1
-            else:
-                g2p_map_v2[t] = 1
-
-
-        aligned_dict.append(word + '\t' + transcr + '\t' + str(aligned))
-
-
-    g2p_map = {}
-    g2p_map['x'] = ['k s']
-    g2p_map['é'] = ['j E', 'j E:']
-
-    for t in g2p_map_v2.keys():
-        if g2p_map_v2[t] > 100:
-            if t[0] in g2p_map:
-                g2p_map[t[0]].append(t[1])
-            else:
-                g2p_map[t[0]] = [t[1]]
-
-    if 'i' in g2p_map:
-        g2p_map['y'] = g2p_map['i']
-    if 'í' in g2p_map:
-        g2p_map['ý'] = g2p_map['í']
-
-    # ensure short and long versions of vowels
-    for grapheme in g2p_map.keys():
-        for p in g2p_map[grapheme]:
-            if p in SHORT_VOWELS:
-                if p + ':' not in g2p_map[grapheme]:
-                    g2p_map[grapheme].append(p + ':')
-            elif p in LONG_VOWELS:
-                short = p.replace(':', '')
-                if short not in g2p_map[grapheme]:
-                    g2p_map[grapheme].append(short)
-
-    # manually add dipththongs and h: k for 'hv':
-    g2p_map['ei'] = ['ei', 'ei:']
-    g2p_map['ey'] = ['ei', 'ei:']
-    g2p_map['au'] = ['9Y', '9Y:']
-    g2p_map['hj'] = ['C']
-    g2p_map['hl'] = ['l_0']
-    g2p_map['hr'] = ['r_0']
-    g2p_map['hn'] = ['n_0']
-    g2p_map['sl'] = ['s t l']
-
-    tmp_g2p_map = {}
-    g2p_map_v2 = {}
-    aligned_dict = []
-    for line in pron_dict_in:
-        word, transcr = line.strip().split('\t')
-        aligned = align_g2p(word, transcr, g2p_map)
-
-        for t in aligned:
-            if t in tmp_g2p_map:
-                tmp_g2p_map[t].append(word + '  ---  ' + transcr)
-                """
-                if len(tmp_g2p_map[t]) > 100:
-                    if t[0] in g2p_map:
-                        g2p_map[t[0]].append(t[1])
-                    else:
-                        g2p_map[t[0]] = [t[1]]
-                """
-
             else:
                 tmp_g2p_map[t] = [word + '  ---  ' + transcr]
 
@@ -313,21 +269,18 @@ def main():
                 g2p_map_v2[t] = 1
 
         aligned_dict.append(word + '\t' + transcr + '\t' + str(aligned))
-
 
     for al in aligned_dict:
        print(str(al))
 
-    out = open('alignment_map_train.txt', 'w')
-    for diff in sorted(tmp_g2p_map, key=lambda x: len(tmp_g2p_map[x]), reverse=True):
+    out = open('alignment_map_train_v2.txt', 'w')
+    for word in sorted(tmp_g2p_map, key=lambda x: len(tmp_g2p_map[x]), reverse=True):
         out.write(
-            str(diff) + '\t' + str(len(tmp_g2p_map[diff])) + '\n')
+            str(word) + '\t' + str(len(tmp_g2p_map[word])) + '\n')
         #if len(tmp_g2p_map[diff]) < 20:
         #    print(str(diff) + '\t' + str(tmp_g2p_map[diff]))
         #if len(diff[1]) == 0 and len(tmp_g2p_map[diff]) < 100:
         #    print(str(diff) + '\t' + str(tmp_g2p_map[diff]))
-
-
 
 
 if __name__ == '__main__':
